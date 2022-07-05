cc2dbb0d64d84d4a5f7b1b8b1987a43fd84643a9
timeester3648 <45339455+timeester3648@users.noreply.github.com>
2022-07-05 15:19:14 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+include "../../premake/common_premake_defines.lua"
+
+project "spirv_headers"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	defines {
+		"ENABLE_OPT",
+		"ENABLE_HLSL"
+	}
+
+	files {
+		"./**.h",
+		"./**.hpp",
+		"./**.cpp"
+	}
+
+ 	filter "configurations:Debug"
+		defines { "MLE_DEBUG_BUILD", "DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
+
+	filter "configurations:Distribution"
+		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
