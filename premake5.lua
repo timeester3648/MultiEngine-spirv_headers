a4ba40c1fa4b4bb152d4d9628c2bb8755abae4be
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:31 +0100

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -18,22 +18,3 @@ project "spirv_headers"
 		"./**.hpp",
 		"./**.hpp11",
 	}
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
